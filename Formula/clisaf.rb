5e725f3178779ebaa0a578549de4ad94c4610ecf
Yousaf Nabi <yousafn@gmail.com>
2022-04-18 21:32:08 +0100

    formula with linux and macos

--- Formula/clisaf.rb
+++ Formula/clisaf.rb
@@ -9,21 +9,23 @@ class Clisaf < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-macos-arm64.gz"
-      sha256 "bbadaef436a9002aea2268beedcda181877f94b1c107d1c26bdea084081aac35"
       bin.install "bundle/main-macos-arm64" => "clisaf"
+      sha256 "ba9a8a41a7ca4fde5e43c94df2e72d4b2d02f48d1d31abe793050c7211e302f3"
     else
       url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-macos-x64.gz"
+      sha256 "bbadaef436a9002aea2268beedcda181877f94b1c107d1c26bdea084081aac35"
       bin.install "bundle/main-macos-x64" => "clisaf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-x64.gz"
-      sha256 "bbadaef436a9002aea2268beedcda181877f94b1c107d1c26bdea084081aac35"
+      url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-arm64.gz"
+      sha256 "ac5f389b1e7b1141cc19e4ebadba771f7a12ac34e76fb5aa0146185c50e9bdb7"
       bin.install "bundle/main-linux-arm64" => "clisaf"
     else
       url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-x64.gz"
+      sha256 "06a6e3d6546ab169ab4047b7c54ea03ee9c559594ca06b7ab0a1b52eef6876be"
       bin.install "bundle/main-linux-x64" => "clisaf"
     end
   end
