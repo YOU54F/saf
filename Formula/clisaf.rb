8075f64401484c18cf9499f2a261ad2e635ca66f
Yousaf Nabi <yousafn@gmail.com>
2022-04-18 22:34:39 +0100

    lint updates

--- Formula/clisaf.rb
+++ Formula/clisaf.rb
@@ -1,24 +1,22 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Clisaf < Formula
-  desc "pronounced silly saf"
-  homepage "https://github.com/YOU54F/clisaf"
-  version "0.0.1"
-
+  desc 'Downloads clisaf via Homebrew'
+  homepage 'https://github.com/YOU54F/clisaf'
+  
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-macos-arm64.gz"
-      sha256 "ba9a8a41a7ca4fde5e43c94df2e72d4b2d02f48d1d31abe793050c7211e302f3"
+      url 'https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-macos-arm64.gz'
+      version '0.0.1'
+      sha256 'ba9a8a41a7ca4fde5e43c94df2e72d4b2d02f48d1d31abe793050c7211e302f3'
       def install
-        bin.install "bundle/main-macos-arm64" => "clisaf"
+        bin.install 'bundle/main-macos-arm64' => 'clisaf'
         puts "# Run 'clisaf'"
       end
     else
-      url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-macos-x64.gz"
-      sha256 "bbadaef436a9002aea2268beedcda181877f94b1c107d1c26bdea084081aac35"
+      url 'https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-macos-x64.gz'
+      version '0.0.1'
+      sha256 'bbadaef436a9002aea2268beedcda181877f94b1c107d1c26bdea084081aac35'
       def install
-        bin.install "bundle/main-macos-x64" => "clisaf"
+        bin.install 'bundle/main-macos-x64' => 'clisaf'
         puts "# Run 'clisaf'"
       end
     end
@@ -26,22 +24,23 @@ class Clisaf < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-arm64.gz"
-      sha256 "ac5f389b1e7b1141cc19e4ebadba771f7a12ac34e76fb5aa0146185c50e9bdb7"
+      url 'https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-arm64.gz'
+      version '0.0.1'
+      sha256 'ac5f389b1e7b1141cc19e4ebadba771f7a12ac34e76fb5aa0146185c50e9bdb7'
       def install
-        bin.install "bundle/main-linux-arm64" => "clisaf"
+        bin.install 'bundle/main-linux-arm64' => 'clisaf'
         puts "# Run 'clisaf'"
       end
     else
-      url "https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-x64.gz"
-      sha256 "06a6e3d6546ab169ab4047b7c54ea03ee9c559594ca06b7ab0a1b52eef6876be"
+      url 'https://github.com/YOU54F/clisaf/releases/download/v0.0.1/main-linux-x64.gz'
+      version '0.0.1'
+      sha256 '06a6e3d6546ab169ab4047b7c54ea03ee9c559594ca06b7ab0a1b52eef6876be'
       def install
-        bin.install "bundle/main-linux-x64" => "clisaf"
+        bin.install 'bundle/main-linux-x64' => 'clisaf'
         puts "# Run 'clisaf'"
       end
     end
   end
-
 
   test do
     system "#{bin}/clisaf"
